62abe54a85805315e13e24857c529990912b23eb
Bright Agbor <brightagboragbor@gmail.com>
2023-09-07 19:59:35 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+/**
+ * Main function to print a message.
+ * Returns 0 to indicate successful execution.
+ */
 int main(void){ 
-puts("Programming is like building a multilingual puzzle\n");
+puts('"Programming is like building a multilingual puzzle\n');
 return (0);
 }
