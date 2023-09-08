9e1f94c945476a80f3272831d04f9d6f4ad4d8f9
Bright Agbor <brightagboragbor@gmail.com>
2023-09-07 20:11:59 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+* This is the main function that prints a message.
+* It returns 0 to indicate successful execution.
+*/
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
