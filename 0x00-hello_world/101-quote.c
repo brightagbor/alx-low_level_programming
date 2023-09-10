5e021a22e35478b3e02bbc0d9d6b214251150c49
Bright Agbor <brightagboragbor@gmail.com>
2023-09-09 18:40:19 -0700

    File 5, 6, 100 101 added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ * It returns 0 to indicate successful execution.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+
+	return (1);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ * It returns 0 to indicate successful execution.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
