281827c2159c51bf12b493abd3bdfb35be1cf96d
Bright Agbor <brightagboragbor@gmail.com>
2023-09-09 20:24:14 -0700

    Updated C files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
@@ -13,6 +12,7 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, message, 59);
 
 	return (1);
