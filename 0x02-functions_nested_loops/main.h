df319b94023eb73b4003722808c1ae384363e987
Bright Agbor <brightagboragbor@gmail.com>
2023-09-13 14:00:26 -0700

    Updating the 1-alphabet.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,16 +8,6 @@
 */
 
 int _putchar(char c);
-void print_alphabet(void)
-{
-        char letter = 'a';
+void print_alphabet(void);
 
-        while (letter <= 'z')
-        {
-                _putchar(letter);
-                letter++;
-        }
-
-        _putchar('\n');
-}
 #endif /* MAIN_H */
