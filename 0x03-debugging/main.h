4cdc00351ff7bf13cdd7cf1fb9b5758d30ec8314
Bright Agbor <brightagboragbor@gmail.com>
2023-09-14 08:53:16 -0700

    Updated script for the header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /**
  * Description: This is a header file which contains all
  * functions declarations in this task
@@ -8,16 +10,6 @@
 */
 
 int positive_or_negative(int i);
-
-/**
- * largest_number - Finds the largest among three integers.
- * @a: The first integer.
- * @b: The second integer.
- * @c: The third integer.
- *
- * Return: The largest of the three integers.
- */
-
 int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
