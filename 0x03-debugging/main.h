6501f2104d278e7788874b5dc4ff3434e644c2c4
Bright Agbor <brightagboragbor@gmail.com>
2023-09-14 06:20:41 -0700

    Updated the header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+/**
+ * Description: This is a header file which contains all
+ * functions declarations in this task
+ *
+*/
+
 int positive_or_negative(int i);
 
 #endif /* MAIN_H */
