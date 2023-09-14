38b67beeec8af9d95c235b85901c99981151178c
Bright Agbor <brightagboragbor@gmail.com>
2023-09-14 09:05:18 -0700

    YUpdated file 3-print_remaining_days.c file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,4 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -16,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
     {
         if (month >= 2 && day >= 60)
         {
-            day++;
+		day++;
         }
 
         printf("Day of the year: %d\n", day);
