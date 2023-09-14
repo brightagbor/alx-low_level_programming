94ef474b12a29be3260309c486087cedbe34236c
Bright Agbor <brightagboragbor@gmail.com>
2023-09-14 06:05:16 -0700

    Updated Script

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,8 +2,6 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*
-* Description: This function checks if a number is positive or negative
 * Return: 0
 */
 
@@ -13,6 +11,6 @@ int main(void)
 
 	i = 98;
 	positive_or_negative(i);
-
+	
 	return (0);
 }
