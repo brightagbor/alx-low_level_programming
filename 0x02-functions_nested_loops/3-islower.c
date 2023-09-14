74236ded0f380cd14a9b7b20d090e7a758676dfb
Bright Agbor <brightagboragbor@gmail.com>
2023-09-13 21:21:59 -0700

    Updated _lower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Main Function
+ *
+ * Description: This function checks if a character is lower
+ *
+ * Return: 0(if not lower) and 1 (if lower)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
