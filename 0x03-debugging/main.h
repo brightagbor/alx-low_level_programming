7c52e0346374fcb3f2b60349a1edc4fbd774dd79
Bright Agbor <brightagboragbor@gmail.com>
2023-09-14 05:03:35 -0700

    Updated file 0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,33 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int positive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
-		printf("%d is zero\n", n);
-	}
-
-	return (0);
-
-}
-
+int positive_or_negative(int i);
 
 
 #endif /* MAIN_H */
