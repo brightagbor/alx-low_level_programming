1163f9350c6d50b4933d3fd149074c50985b2d59
Bright Agbor <brightagboragbor@gmail.com>
2023-09-18 19:55:49 -0700

    Updated 8-print_array.c file and other files with whitespace issues

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,7 +17,6 @@ void print_rev(char *s)
 	{
 		length++;
 	}
-	
 	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -21,7 +21,7 @@ void rev_string(char *s)
 	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
 		char temp = s[i];
-	
+		
 		s[i] = s[j];
 		s[j] = temp;
 	}
