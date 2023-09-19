75a59ae3089fb2531158f8530838b464fd2295e4
Bright Agbor <brightagboragbor@gmail.com>
2023-09-18 17:58:09 -0700

    2-strlen.c file created

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen- main function
+ *
+ * Description: The function returns the length of a string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
