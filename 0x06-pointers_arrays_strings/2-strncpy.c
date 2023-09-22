dfee93b63b04cf4f235b89367c5cc0bb4c59bd22
Bright Agbor <brightagboragbor@gmail.com>
2023-09-22 12:54:29 -0700

    Updating all files with issues

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -27,5 +27,5 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	*dest = '\0';
 
-	return dest;
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -24,6 +24,7 @@ int _strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
+
 	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (0);
