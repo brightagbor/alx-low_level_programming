9d2363d53f7be75923a97adced6820681babda16
Bright Agbor <brightagboragbor@gmail.com>
2023-10-01 18:58:46 -0700

    Updated file 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+ * str_length - main function
+ * @s: Pointer to char type
+ * Description: The function returns the length of a string
+ *
+ * Return: int value
+ */
+int str_length(char *s)
+{
+	if (*s == '\0')
+		return 0;
+	return (1 + str_length(s + 1));
+}
+
+/**
+ * check_palindrome - main function
+ * @start: Pointer to char type
+ * @end: Pointer to char type
+ * Description: The function returns the length of a string
+ *
+ * Return: int value
+ */
+int check_palindrome(char *start, char *end)
+{
+	if (start >= end)
+		retun (1);
+	if (*start != *end)
+		return (0);
+
+	return (check_palindrome(start + 1, end - 1));
+}
+/**
  * is_palindrome - main function
  * @s: Pointer to char type
  * Description: The function returns 1 if a string is a palindrome and 0 if not
@@ -9,19 +40,10 @@
  */
 int is_palindrome(char *s)
 {
-	int length = 0;
-	int i, j;
+	int length = str_length(s);
 
-	while (s[length] != '\0')
-		length++;
 	if (length == 0)
 		return (1);
 
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-			return (0);
-	}
-
-	return (1);
+	return (check_palindrome(s, s + length - 1));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 /**
  * wildcmp - main function
  * @s1: Pointer to char type
- * @s2: Pointer to char type 
+ * @s2: Pointer to char type
  * Description: The function compares two strings
  *
  * Return: int value
@@ -15,12 +15,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
-			return wildcmp(s1, s2 + 1);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 +1, s2))
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	if (*s1 == *s2 || *s2 == '?')
-		return wildcmp(s1 +1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	return (0);
 }
