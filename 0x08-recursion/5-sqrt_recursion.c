3169b1728feff27d7f97ebc3549f8e8549e2c116
Bright Agbor <brightagboragbor@gmail.com>
2023-10-01 21:03:34 -0700

    File number 5 updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,41 @@
 #include "main.h"
 
 /**
- * _pow_recursion - main function
+ * _sqrt_helper - main function
  * @n: Integer variable
  * @start: Integer variable
  * @end: Integer variable
- * Description: The function returns the natural square root of a number
+ * Description: The function returns square root helper
  *
  * Return: integer
  */
-int _sqrt_recursion(int x, int y)
+int _sqrt_helper(int n, int start, int end);
+{
+	int mid = start + (end - start) / 2;
+	long long square = (long long)mid * mid;
+
+	if (start > end)
+		return (-1);
+	if (square == n)
+		return (mid);
+	else if (square < n)
+		return (_sqrt_helper(n, mid + 1, end));
+	else
+		return (_sqrt_helper(n, start, mid - 1));
+
+}
+
+/**
+ * _sqrt_recursion- main function
+ * @n: Integer variable
+ * Description: The function returns the square root of a number
+ *
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	
+	return (_sqrt_helper(n, 0, n));
 }
