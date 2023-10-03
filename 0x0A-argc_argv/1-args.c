b1c0eba30b45a80096edf27487dac3ce11a7fce0
Bright Agbor <brightagboragbor@gmail.com>
2023-10-02 20:57:21 -0700

    Added file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Description: The function prints the number of arguments passed into it
+ *
+ * Return: The return value is int.
+ */
+int main(int argc, char *argv[])
+{
+	printf("Number of arguments: %d\n", argc - 1);
+	return 0;
+}
